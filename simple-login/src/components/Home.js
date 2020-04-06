3913e609cceb5c3025d8c4184ef0ff4206fad808
hnishi <nishigami2509@gmail.com>
2020-04-06 19:26:58 +0900

    render page dependent on token

--- simple-login/src/components/Home.js
+++ simple-login/src/components/Home.js
@@ -1,23 +1,58 @@
 import React, {useState} from 'react';
-import {useHistory} from 'react-router-dom';
+import { BrowserRouter, Route, useHistory } from "react-router-dom";
+import Login from "./Login";
+
+function LoginPage() {
+  const history = useHistory();
+  const handleLogoutSubmit = (e) => {
+      e.preventDefault();
+      sessionStorage.setItem('auth-token', 'logout');
+      history.push('/');
+  }
+
+  return (
+    <div className="login-page">
+        <h1>Home</h1>
+        <p>You successfully logged in !</p>
+        <h2>Log Out</h2>
+        <form autoComplete="off" onSubmit={handleLogoutSubmit}>
+            <button type="submit" className="btn btn-primary">
+                Logout
+            </button>
+        </form>
+    </div>
+  );
+}
+
+function NotLoginPage() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
 
 function Home() {
     const history = useHistory();
     let element01 = 'debug1';
+    let page_home;
     if (!sessionStorage.getItem('auth-token')) {
         console.log('no auth token set');
         element01 = 'debug2';
         //do something like redirect to login page
+        page_home = <NotLoginPage />;
     } else {
         const authToken = '123456abcdef';
         if (sessionStorage.getItem('auth-token') === authToken) {
             console.log('good token. Log in.');
             //do something like redirect to todo page
             element01 = 'debug3';
+            page_home = <LoginPage />;
         } else {
             console.log('bad token.');
             //do something like redirect to login page
             element01 = 'debug4';
+            page_home = <NotLoginPage />;
         }
     }
     console.log(element01);
@@ -28,15 +63,17 @@ function Home() {
         history.push('/');
     }
 
+    //<div className="login-page">
+    //    <h2>Log Out</h2>
+    //    <form autoComplete="off" onSubmit={handleLogoutSubmit}>
+    //        <button type="submit" className="btn btn-primary">
+    //            Logout
+    //        </button>
+    //    </form>
+    //</div>
     return(
-    <div className="login-page">
-        <p> hello </p>
-        <h2>Log Out</h2>
-        <form autoComplete="off" onSubmit={handleLogoutSubmit}>
-            <button type="submit" className="btn btn-primary">
-                Logout
-            </button>
-        </form>
+    <div>
+        {page_home}
     </div>
     );
 }
